c061fe879fa02243f07a91fd9eb9a48d31dada5e
Ethan Isola <isolaethan@gmail.com>
2021-12-27 22:49:05 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {name:"Ethan", streetAddress:"64 Concord"};
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmp = {...employee};
+    newEmp[key] = value;
+    return newEmp;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    const newEmp = employee;
+    newEmp[key] = value;
+    return newEmp;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const newEmp = {...employee};
+    delete newEmp[key];
+    return newEmp;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    const newEmp = employee;
+    delete newEmp[key];
+    return newEmp;
+
+}
